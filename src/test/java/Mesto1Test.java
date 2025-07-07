20ad3877d8cbc46fc0d74519c2537f83ce2db616
Anastasia Parunova <parunovaanastasiia@gmail.com>
2025-07-07 14:27:53 +0200

    test updated

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -16,17 +16,7 @@ public class Mesto1Test {
         RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
     }
 
-    @Test
-    @DisplayName("Add a new photo")
-    @Description("This test is for adding a new photo to Mesto.")
-    void addNewPhoto() {
-        given()
-                .header("Content-type", "application/json") // Передаём Content-type в заголовке для указания типа файла
-                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
-                .body("{\"name\":\"Москва\",\"link\":\"https://code.s3.yandex.net/qa-automation-engineer/java/files/paid-track/sprint1/photoSelenium.jpg\"}") // Формируем тело запроса
-                .post("/api/cards") // Делаем POST-запрос
-                .then().statusCode(401); // Проверяем код ответа
-    }
+
 
     @Test
     @DisplayName("Like the first photo")
